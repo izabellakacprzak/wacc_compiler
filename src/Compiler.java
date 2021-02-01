e4e3a1c114e5cb9a42cdb6b3dc472211ca3ee261
Kacprzak, Izabella <izabella.kacprzak19@imperial.ac.uk>
2021-02-01 08:27:29 +0100

    Added creating parse tree in Compiler [IK]

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,8 +1,7 @@
-import antlr.WACCParser.ProgramContext;
 import java.io.IOException;
 import org.antlr.v4.runtime.*;
-import antlr.WACCLexer;
-import antlr.WACCParser;
+import org.antlr.v4.runtime.tree.*;
+import antlr.*;
 public class Compiler {
 
   public static void main(String[] args){
@@ -11,7 +10,6 @@ public class Compiler {
     }
 
     CharStream input = null;
-
     try{
       input = CharStreams.fromFileName(args[0]);
 
@@ -22,11 +20,15 @@ public class Compiler {
     WACCLexer lexer = new WACCLexer(input);
     lexer.removeErrorListeners();
     // add our own error  ??
-    TokenStream tokens = new CommonTokenStream(lexer);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+
     WACCParser parser = new WACCParser(tokens);
+
     parser.removeErrorListeners();
     // add our own error listener
-    ProgramContext context = parser.program();
+    ParseTree tree = parser.program();
+
+    System.out.println(tree.toStringTree(parser));
 
     // check if error listener encountered any errors and if so exit with an error code
     // otherwise continue with semantics checking
