a72031c3908263d56647a8f0bbd7b5464243f0f9
no119 <no119@ic.ac.uk>
2021-02-10 13:26:00 +0000

    Edited shell script to include semantic error checking [BG, IK, NO, UM]

--- run_tests.sh
+++ run_tests.sh
@@ -23,12 +23,16 @@ run_tests () {
 
 VALID_TESTS=src/test/valid
 SYNTAX_ERROR_TESTS=src/test/invalid/syntaxErr
+SEMANTIC_ERROR_TESTS=src/test/invalid/semanticErr
 SUCCESS_CODE=0
 SYNTAX_ERROR_CODE=100
+SEMANTIC_ERROR_CODE=200
 runValidTests=0
 runSyntaxErrorTests=0
+runSemanticErrorTests=0
 passedValidTests=0
 passedSyntaxErrorTests=0
+passedSemanticErrorTests=0
 
 echo Running valid tests...
 run_tests "$VALID_TESTS" $SUCCESS_CODE
@@ -45,12 +49,25 @@ echo passed $passedTests / $runTests syntax error tests
 runSyntaxErrorTests=$runTests
 passedSyntaxErrorTests=$passedTests
 
+
+echo =======================
+passedTests=0
+runTests=0
+echo Running semantic error tests...
+run_tests "$SEMANTIC_ERROR_TESTS" $SEMANTIC_ERROR_CODE
+echo passed $passedTests / $runTests semantic error tests
+runSemanticErrorTests=$runTests
+passedSemanticErrorTests=$passedTests
+
 echo =======================
 echo SUMMARY
 echo passed $passedValidTests / $runValidTests valid tests
 echo passed $passedSyntaxErrorTests / $runSyntaxErrorTests syntax error tests
+echo passed $passedSemanticErrorTests / $runSemanticErrorTests semantic error tests
 
-if [ $passedValidTests -eq $runValidTests ] && [ $passedSyntaxErrorTests -eq $runSyntaxErrorTests ]; then
+
+if [ $passedValidTests -eq $runValidTests ] && [ $passedSyntaxErrorTests -eq $runSyntaxErrorTests ] &&
+       [ $passedSemanticErrorTests -eq $runSemanticErrorTests ]; then
   exit 0
 else
   exit 1
