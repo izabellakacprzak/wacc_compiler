e47066170d9dc648d0d8516bc38678159d96f978
Kacprzak, Izabella <izabella.kacprzak19@imperial.ac.uk>
2021-02-11 12:26:56 +0100

    Refactored run_tests.sh to enable modyfing tests ran and added .txt files with paths to tests [IK]

--- run_tests.sh
+++ run_tests.sh
@@ -1,41 +1,38 @@
 #!/bin/sh
-passedTests=0
-runTests=0
 run_tests () {
-    for pathname in "$1"/*; do
-        if [ -d "$pathname" ]; then
-            run_tests "$pathname" "$2"
-        else
-            case "$pathname" in
-                *.wacc)
-                  echo running test $runTests "$pathname"
-                  ./compile "$pathname" >/dev/null 2>&1
-                  if [ $? -eq $(($2)) ]; then
-                    passedTests=$((passedTests + 1))
-                    echo test $runTests passed
-                  fi
-                  runTests=$((runTests + 1))
-
-            esac
-        fi
+  for line in $1
+    do
+      echo running test "$runTests" "$line"
+      ./compile "$line" >/dev/null 2>&1
+      if [ $? -eq $(($2)) ]; then
+         passedTests=$((passedTests + 1))
+         echo test "$runTests" passed
+      fi
+      runTests=$((runTests + 1))
     done
 }
 
-VALID_TESTS=src/test/valid
-SYNTAX_ERROR_TESTS=src/test/invalid/syntaxErr
-SEMANTIC_ERROR_TESTS=src/test/invalid/semanticErr
+# valid tests variables
+VALID_TESTS=src/test/valid_tests.txt
 SUCCESS_CODE=0
-SYNTAX_ERROR_CODE=100
-SEMANTIC_ERROR_CODE=200
 runValidTests=0
-runSyntaxErrorTests=0
-runSemanticErrorTests=0
 passedValidTests=0
+
+# syntax error tests variables
+SYNTAX_ERROR_TESTS=src/test/syntax_error_tests.txt
+SYNTAX_ERROR_CODE=100
+runSyntaxErrorTests=0
 passedSyntaxErrorTests=0
+
+# sematic error tests variables
+SEMANTIC_ERROR_TESTS=src/test/invalid/semantic_error_tests.txt
+SEMANTIC_ERROR_CODE=200
+runSemanticErrorTests=0
 passedSemanticErrorTests=0
 
 echo Running valid tests...
-run_tests "$VALID_TESTS" $SUCCESS_CODE
+LINES=$(cat $VALID_TESTS)
+run_tests "$LINES" $SUCCESS_CODE
 echo passed $passedTests / $runTests valid tests
 runValidTests=$runTests
 passedValidTests=$passedTests
@@ -44,30 +41,31 @@ echo =======================
 passedTests=0
 runTests=0
 echo Running syntax error tests...
-run_tests "$SYNTAX_ERROR_TESTS" $SYNTAX_ERROR_CODE
+LINES=$(cat $SYNTAX_ERROR_TESTS)
+run_tests "$LINES" $SYNTAX_ERROR_CODE
 echo passed $passedTests / $runTests syntax error tests
 runSyntaxErrorTests=$runTests
 passedSyntaxErrorTests=$passedTests
 
 
-echo =======================
-passedTests=0
-runTests=0
-echo Running semantic error tests...
-run_tests "$SEMANTIC_ERROR_TESTS" $SEMANTIC_ERROR_CODE
-echo passed $passedTests / $runTests semantic error tests
-runSemanticErrorTests=$runTests
-passedSemanticErrorTests=$passedTests
+#echo =======================
+#passedTests=0
+#runTests=0
+#echo Running semantic error tests...
+#LINES=$(cat $SEMANTIC_ERROR_TESTS)
+#run_tests "$LINES" $SEMANTIC_ERROR_CODE
+#echo passed $passedTests / $runTests semantic error tests
+#runSemanticErrorTests=$runTests
+#passedSemanticErrorTests=$passedTests
 
 echo =======================
 echo SUMMARY
 echo passed $passedValidTests / $runValidTests valid tests
 echo passed $passedSyntaxErrorTests / $runSyntaxErrorTests syntax error tests
-echo passed $passedSemanticErrorTests / $runSemanticErrorTests semantic error tests
+#echo passed $passedSemanticErrorTests / $runSemanticErrorTests semantic error tests
 
 
-if [ $passedValidTests -eq $runValidTests ] && [ $passedSyntaxErrorTests -eq $runSyntaxErrorTests ] &&
-       [ $passedSemanticErrorTests -eq $runSemanticErrorTests ]; then
+if [ $passedSyntaxErrorTests -eq $runSyntaxErrorTests ] && [ $passedValidTests -eq $runValidTests ]; then
   exit 0
 else
   exit 1
