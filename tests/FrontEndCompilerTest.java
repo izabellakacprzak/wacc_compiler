7a045a94d6cb415c25d685eedf2baf61275ea828
Bianca-Mihaela Ganescu <bg319@ic.ac.uk>
2021-02-12 12:41:26 +0000

    changed FrontEndCompilerTest to have 3 different test methods running ./run_tests.sh for the syntax error, semantic error and valid cases respectively [BG]

--- tests/FrontEndCompilerTest.java
+++ tests/FrontEndCompilerTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 
 public class FrontEndCompilerTest {
   @Test
-  public void runTests() {
+  public void runValidTests() {
     try {
-      String[] cmds = {"./run_tests.sh"};
+      String[] cmds = {"./run_tests.sh", "valid", "src/test/valid_tests.txt", "0"};
       Process process = new ProcessBuilder(cmds).inheritIO().start();
       process.waitFor();
       int exit = process.exitValue();
@@ -16,4 +16,30 @@ public class FrontEndCompilerTest {
     }
   }
 
+
+  @Test
+  public void runSyntaxErrorTests() {
+    try {
+      String[] cmds = {"./run_tests.sh", "syntax error", "src/test/syntax_error_tests.txt", "100"};
+      Process process = new ProcessBuilder(cmds).inheritIO().start();
+      process.waitFor();
+      int exit = process.exitValue();
+      assert (exit == 0);
+    } catch (IOException | InterruptedException e) {
+      System.out.println("Exception");
+    }
+  }
+
+  @Test
+  public void runSemanticErrorTests() {
+    try {
+      String[] cmds = {"./run_tests.sh", "semantic error", "src/test/semantic_error_tests.txt", "200"};
+      Process process = new ProcessBuilder(cmds).inheritIO().start();
+      process.waitFor();
+      int exit = process.exitValue();
+      assert (exit == 0);
+    } catch (IOException | InterruptedException e) {
+      System.out.println("Exception");
+    }
+  }
 }
