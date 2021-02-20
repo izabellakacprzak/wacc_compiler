0fa2fd2e44dd5d0a7196d945badc17a67b6bcb13
Kacprzak, Izabella <izabella.kacprzak19@imperial.ac.uk>
2021-02-20 17:03:02 +0100

    corrected path in test [UM, IK]

--- tests/BackEndCompilerTest.java
+++ tests/BackEndCompilerTest.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import org.junit.Test;
+import org.hamcrest.*;
 
 public class BackEndCompilerTest {
   private String[] compileCmds = {"./compile"};
@@ -10,7 +12,7 @@ public class BackEndCompilerTest {
 
   @Test
   public void runBackendTests() {
-    File waccFiles = new File("../src/test/valid");
+    File waccFiles = new File("src/test/valid");
     listOfFiles(waccFiles);
   }
 
@@ -19,13 +21,13 @@ public class BackEndCompilerTest {
     for (File file : filesList) {
       if (file.isFile()) {
         try {
-          Process compileProcess = new ProcessBuilder(compileCmds + file.getAbsolutePath()).start();
+          Process compileProcess = new ProcessBuilder(Arrays.toString(compileCmds) + file.getAbsolutePath()).start();
           compileProcess.wait();
           String assemblyFile = file.getName().replace(".wacc", ".s");
-          Process assebleProcess = new ProcessBuilder(assembleCmds + assemblyFile).start();
+          Process assebleProcess = new ProcessBuilder(Arrays.toString(assembleCmds) + assemblyFile).start();
           assebleProcess.wait();
           String emulateFile = file.getName().replace(".wacc", "");
-          Process emulateProcess = new ProcessBuilder(emulateCmds + emulateFile).start();
+          Process emulateProcess = new ProcessBuilder(Arrays.toString(emulateCmds) + emulateFile).start();
           emulateProcess.wait();
           // check for output and exit code
         } catch (IOException | InterruptedException e) {
