cc4e76c18801077c2c568ca64ed2edc0c42e7f46
Bianca-Mihaela Ganescu <bg319@ic.ac.uk>
2021-02-23 15:28:23 +0000

    Added implementation for the conditional assembly instructions in IR and printer object for the IR of assembly instructions [BG]

---
+++ src/InternalRepresentation/ConditionCode.java
@@ -0,0 +1,38 @@
+package InternalRepresentation;
+
+public class ConditionCode {
+  private final Condition condition;
+
+  public ConditionCode(Condition condition) {
+    this.condition = condition;
+  }
+
+
+  public String getCondName() {
+    return condition.name();
+  }
+
+  /*
+  L Link
+  EQ Equal
+  NE Not equal
+  HS/CS Unsigned higher or same, carry set
+  LO/CC Unsigned lower, carry clear
+  MI Negative, minus
+  PL Positive or zero, plus
+  VS Overflow
+  VC No overflow
+  HI Unsigned higher
+
+  LS Unsigned lower or same
+  GE Signed greater or equal
+  LT Signed less than
+  GT Signed greater than
+  LE Signed less than or equal
+  AL Always
+  */
+  public enum Condition {
+    L, EQ, NE, CS, VS, GE, LT, GT, LE
+  }
+}
+
---
+++ src/InternalRepresentation/LDR.java
@@ -0,0 +1,85 @@
+package InternalRepresentation;
+
+public class LDR implements Instruction {
+  private final Register destReg;
+  private ConditionCode conditionCode;
+  private int immOffset;
+  private Register srcReg;
+  private String constant = null;
+  private boolean isLDRSB = false; //signed byte
+
+  private final InstructionPrinter printer = new InstructionPrinter();
+
+  //SIMPLE INSTR + DST REG + STRING CONSTANT
+  public LDR(Register destReg, String constant) {
+    this.destReg = destReg;
+    this.constant = constant;
+  }
+
+  //SIMPLE INSTR + DST REG + INT CONSTANT
+  public LDR(Register destReg, int constant) {
+    this.destReg = destReg;
+    this.constant = Integer.toString(constant);
+  }
+
+
+  //COND INSTR + DST REG + STRING CONSTANT
+  public LDR(ConditionCode conditionCode, Register destReg, String constant) {
+    this.conditionCode = conditionCode;
+    this.destReg = destReg;
+    this.constant = constant;
+  }
+
+  //COND INSTR + DST REG + INT CONSTANT
+  public LDR(ConditionCode conditionCode, Register destReg, int constant) {
+    this.conditionCode = conditionCode;
+    this.destReg = destReg;
+    this.constant = Integer.toString(constant);
+  }
+
+
+  //SIMPLE INSTR + DST REG + SRC REG
+  public LDR(Register destReg, Register srcReg) {
+    this.destReg = destReg;
+    this.srcReg = srcReg;
+    this.immOffset = 0;
+  }
+
+  //SIMPLE INSTR + DST REG + SRC REG + OFFSET
+  public LDR(Register destReg, Register srcReg, int immOffset) {
+    this.destReg = destReg;
+    this.srcReg = srcReg;
+    this.immOffset = immOffset;
+  }
+
+
+  //COND INSTR + DST REG + SRC REG
+  public LDR(ConditionCode conditionCode, Register destReg, Register srcReg) {
+    this.conditionCode = conditionCode;
+    this.destReg = destReg;
+    this.srcReg = srcReg;
+    this.immOffset = 0;
+  }
+
+  //COND INSTR + DST REG + SRC REG + OFFSET
+  public LDR(ConditionCode conditionCode, Register destReg, Register srcReg, int immOffset) {
+    this.conditionCode = conditionCode;
+    this.destReg = destReg;
+    this.srcReg = srcReg;
+    this.immOffset = immOffset;
+  }
+
+  public void setIsLDRSB() {
+    isLDRSB = true;
+  }
+
+  @Override
+  public String writeInstruction() {
+    return printer.printLDR(destReg,
+        conditionCode,
+        immOffset,
+        srcReg,
+        constant,
+        isLDRSB);
+  }
+}
