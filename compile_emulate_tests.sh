bcb827d7004af2d9d06f9eb399af6d2aaf3199a6
Kacprzak, Izabella <izabella.kacprzak19@imperial.ac.uk>
2021-03-03 16:21:33 +0000

    refactored compile_emulate_tests.sh [NO, IK]

--- compile_emulate_tests.sh
+++ compile_emulate_tests.sh
@@ -1,5 +1,62 @@
 #!/bin/bash
 
+execute() {
+  pathname=$1
+  echo running test $runTests "$pathname"
+  flag=true
+
+	refOutput=${pathname/valid/backendTests}
+
+	if [ ! -e ${refOutput/.wacc/.txt} ]; then
+	  echo $'\n' | ./refCompile "-x" "$pathname" > ${refOutput/.wacc/.txt}
+	  fi
+
+  ./compile "$pathname" "-a" >/dev/null 2>&1
+  arm-linux-gnueabi-gcc -o ${pathname/.wacc/} -mcpu=arm1176jzf-s -mtune=arm1176jzf-s ${pathname/.wacc/.s}
+  qemu-arm -L /usr/arm-linux-gnueabi/ ${pathname/.wacc/} > output.txt
+  ourExitCode=$?
+  rm ${pathname/.wacc/.s}
+  rm ${pathname/.wacc/}
+
+  { read -r;
+    correctFlag=false
+    while read -r refLine;
+      do
+        if "$flag" && [ "$refLine" = "===========================================================" ]; then
+          flag=false
+        fi
+        flag=true
+        { read -r;
+        while "$correctFlag" && "$flag" && read -r refLine;
+        do
+          if "$flag" && [ "$refLine" = "===========================================================" ]; then
+            flag=false
+            read -r refLine
+          fi
+          if [ ! $flag ]; then
+            #compare outputs
+            ourLine=$(read -r ${pathname/valid/backendTests});
+            if [ "$refLine" != "$ourLine" ]; then
+              correctFlag=false
+            fi
+          fi
+        done; } < ${refOutput/.wacc/.txt}
+
+        #compare exit code
+        if [ "$refLine" = "The exit code is ${ourExitCode}." ] ; then
+          correctFlag=true
+        fi
+      done; } < ${refOutput/.wacc/.txt}
+
+      if [ $correctFlag ]; then
+        passedTests=$((passedTests + 1))
+        echo -e "$GREEN"passed"$NC": test "$runTests" "$pathname"
+      else
+        echo -e "$RED"failed"$NC": test "$runTests" "$pathname"
+      fi
+      runTests=$((runTests + 1))
+}
+
 run_test_in_dir() {
   for pathname in "$1"/*; do
         if [ -d "$pathname" ]; then
@@ -7,59 +64,7 @@ run_test_in_dir() {
         else
             case "$pathname" in
                 *.wacc)
-                  echo running test $runTests "$pathname"
-                  flag=true
-
-		              refOutput=${pathname/valid/backendTests}
-
-		              if [ ! -e ${refOutput/.wacc/.txt} ]; then
-		                echo $'\n' | ./refCompile "-x" "$pathname" > ${refOutput/.wacc/.txt}
-		              fi
-
-                  ./compile "$pathname" "-a">/dev/null 2>&1
-                  arm-linux-gnueabi-gcc -o ${pathname/.wacc/} -mcpu=arm1176jzf-s -mtune=arm1176jzf-s ${pathname/.wacc/.s}
-                  qemu-arm -L /usr/arm-linux-gnueabi/ ${pathname/.wacc/} > output.txt
-                  ourExitCode=$?
-                  rm ${pathname/.wacc/.s}
-                  rm ${pathname/.wacc/}
-
-                  { read -r;
-                  correctFlag=true
-                  while read -r refLine;
-                    do
-                     if "$flag" && [ "$refLine" = "===========================================================" ]; then
-                        flag=false
-                      fi
-                      flag=true
-                      { read -r;
-                      while "$correctFlag" && "$flag" && read -r refLine;
-                        do
-                          if "$flag" && [ "$refLine" = "===========================================================" ]; then
-                            flag=false
-                            read -r refLine
-                          fi
-                          if [ ! $flag ]; then
-                            #compare outputs
-                            ourLine=$(read -r ${pathname/valid/backendTests});
-                            if [ "$refLine" != "$ourLine" ]; then
-                              correctFlag=false
-                            fi
-                          fi
-                        done; } < ${refOutput/.wacc/.txt}
-
-                      #compare exit code
-                      if [ "$refLine" != "The exit code is ${ourExitCode}." ] ; then
-                        correctFlag=false
-                      fi
-                    done; } < ${refOutput/.wacc/.txt}
-
-                   if [ $correctFlag ]; then
-                      passedTests=$((passedTests + 1))
-                      echo -e "$GREEN"passed"$NC": test "$runTests" "$pathname"
-                    else
-                      echo -e "$RED"failed"$NC": test "$runTests" "$pathname"
-                    fi
-                    runTests=$((runTests + 1))
+                  execute "$pathname"
             esac
         fi
     done
@@ -72,57 +77,7 @@ run_tests () {
         else
             case "$pathname" in
                 *.wacc)
-                  echo running test $runTests "$pathname"
-                  flag=true
-
-		              if not[ -e ${refOutput/.wacc/.txt} ]; then
-		                ./refCompile "-x" "$pathname" > ${refOutput/.wacc/.txt}
-		              fi
-
-                  ./compile "$pathname" "-a">/dev/null 2>&1
-                  arm-linux-gnueabi-gcc -o ${pathname/.wacc/} -mcpu=arm1176jzf-s -mtune=arm1176jzf-s ${pathname/.wacc/.s}
-                  qemu-arm -L /usr/arm-linux-gnueabi/ ${pathname/.wacc/} > output.txt
-                  ourExitCode=$?
-                  rm ${pathname/.wacc/.s}
-                  rm ${pathname/.wacc/}
-
-                  { read -r;
-                  correctFlag=true
-                  while read -r refLine;
-                    do
-                      if "$flag" && [ "$refLine" = "===========================================================" ]; then
-                        flag=false
-                      fi
-                      flag=true
-                      { read -r;
-                      while "$correctFlag" && "$flag" && read -r refLine;
-                        do
-                          if "$flag" && [ "$refLine" = "===========================================================" ]; then
-                            flag=false
-                            read -r refLine
-                          fi
-                          if [ ! $flag ]; then
-                            #compare outputs
-                            ourLine=$(read -r ${pathname/valid/backendTests});
-                            if [ "$refLine" != "$ourLine" ]; then
-                              correctFlag=false
-                            fi
-                          fi
-                        done; } < ${refOutput/.wacc/.txt}
-
-                      #compare exit code
-                      if [ "$refLine" != "The exit code is ${ourExitCode}." ] ; then
-                        correctFlag=false
-                      fi
-                    done; } < ${refOutput/.wacc/.txt}
-
-                    if [ $correctFlag ]; then
-                      passedTests=$((passedTests + 1))
-                      echo -e "$GREEN"passed"$NC": test "$runTests" "$pathname"
-                    else
-                      echo -e "$RED"failed"$NC": test "$runTests" "$pathname"
-                    fi
-                    runTests=$((runTests + 1))
+                  execute "$pathname"
             esac
         fi
     done
